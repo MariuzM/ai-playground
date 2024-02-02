ac4e7065cc08037223e24f58d7e6555265d7e925
MariuzM <info@marius.dev>
2024-02-02 18:10:01 +0700

    chore: clean

--- ai-python/test.py
+++ ai-python/test.py
@@ -1,21 +1,7 @@
 from transformers import pipeline
 
-# Allocate a pipeline for sentiment-analysis
-pipe = pipeline('sentiment-analysis')
 
-out = pipe('I love transformers!')
+pipe = pipeline("sentiment-analysis")
+
+out = pipe("I love transformers!")
 print(out)
-# [{'label': 'POSITIVE', 'score': 0.999806941}]
-
-
-
-
-
-
-# import cv2
-
-# img = cv2.imread("img.png")
-
-# cv2.imshow("image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
