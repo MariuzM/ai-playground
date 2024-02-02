0ba2804bd4fb2d5ceb7a2fde18b9c99ef8f80467
MariuzM <info@marius.dev>
2024-02-03 00:43:31 +0700

    fix: temp fix

--- ai-bun/index.ts
+++ ai-bun/index.ts
@@ -1,9 +1,10 @@
-import { pipeline } from '@xenova/transformers';
+import { pipeline } from '@sroussey/transformers';
 
 const pipe = await pipeline(
   'text-classification',
   'Xenova/distilbert-base-uncased-finetuned-sst-2-english'
 );
+
 const out = await pipe('I love transformers!');
 
 console.log('🚀 ~ out:', out);
